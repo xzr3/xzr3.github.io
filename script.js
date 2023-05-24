77513eeb6e9ca42301285e78bfe41dc13a794670
xzr3 <ammaryousaf341@gmail.com>
2023-05-24 18:43:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,121 +2,69 @@ const apiKey = '5948fedc97ad674cabed5c55ee23246f';
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
-const SuggestionGrid = document.getElementById('suggestion-grid');
 let searchResults = [];
 
-// Function to select a random movie or series ID from TMDB API
-async function selectRandomMediaID() {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
-  const response = await fetch(url);
-  const movieData = await response.json();
-  if (movieData.results && movieData.results.length > 0) {
-    const randomIndex = Math.floor(Math.random() * movieData.results.length);
-    return movieData.results[randomIndex].id;
-  }
-
-  const seriesURL = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}`;
-  const seriesResponse = await fetch(seriesURL);
-  const seriesData = await seriesResponse.json();
-  if (seriesData.results && seriesData.results.length > 0) {
-    const randomIndex = Math.floor(Math.random() * seriesData.results.length);
-    return seriesData.results[randomIndex].id;
-  }
-
-  return null;
+// Function to request movie details from the TMDB API
+function getMovieDetails(movieId) {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const movieDetails = {
+        id: data.id,
+        title: data.title,
+        poster_path: data.poster_path,
+        release_date: data.release_date,
+        vote_average: data.vote_average,
+        cast: '', // Add code to get the cast from the API response
+        awards: '', // Add code to get the awards from the API response
+        mediaType: 'movie' // Set the media type to 'movie' for movies
+      };
+      displayMediaDetails(movieDetails);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
 }
 
-// Function to request movie or series details from the TMDB API
-async function getMediaDetails(mediaID, isSearched = false) {
-  const url = `https://api.themoviedb.org/3/movie/${mediaID}?api_key=${apiKey}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  if (response.ok) {
-    const mediaDetails = {
-      imdbID: data.imdb_id,
-      Title: data.title || data.name,
-      Poster: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
-      Type: data.title ? 'movie' : 'series',
-      Year: data.release_date ? data.release_date.substring(0, 4) : 'N/A',
-      Rated: 'N/A',
-      Released: data.release_date ? data.release_date : 'N/A',
-      Genre: data.genres ? data.genres.map((genre) => genre.name).join(', ') : 'N/A',
-      Writer: 'N/A',
-      Actors: 'N/A',
-      Plot: data.overview,
-      Language: data.original_language,
-      Awards: 'N/A',
-      isSearched: isSearched,
-    };
-    displayMediaDetails(mediaDetails);
-  } else {
-    console.log('Error:', data.status_message);
-  }
+// Function to request series details from the TMDB API
+function getSeriesDetails(seriesId) {
+  const url = `https://api.themoviedb.org/3/tv/${seriesId}?api_key=${apiKey}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const seriesDetails = {
+        id: data.id,
+        name: data.name,
+        poster_path: data.poster_path,
+        release_date: data.first_air_date,
+        vote_average: data.vote_average,
+        cast: '', // Add code to get the cast from the API response
+        awards: '', // Add code to get the awards from the API response
+        mediaType: 'series' // Set the media type to 'series' for TV series
+      };
+      displayMediaDetails(seriesDetails);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
 }
 
-// Usage
-async function loadRandomMedia() {
-  const randomMediaID = await selectRandomMediaID();
-  if (randomMediaID) {
-    getMediaDetails(randomMediaID);
-  }
-}
-
-loadRandomMedia();
-
-function displayMediaDetails(details) {
-  const mediaId = details.imdbID;
-  let videoLink;
-
-  if (details.Type === 'movie') {
-    videoLink = `https://2embed.org/embed/movie?imdb=${mediaId}`;
-  } else if (details.Type === 'series') {
-    videoLink = `https://vidsrc.me/embed/${mediaId}`;
-  } else {
-    // Handle other types (if needed)
-    videoLink = '';
-  }
-
-  resultGrid.innerHTML = `
-    <div class="movie-poster">
-      <img src="${details.Poster !== 'N/A' ? details.Poster : 'image_not_found.png'}" alt="movie poster">
-    </div>
-    <div class="movie-info">
-      <h3 class="movie-title">${details.Title}</h3>
-      ${videoLink ? `<iframe width="100%" height="270" src="${videoLink}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>` : ''}
-      <ul class="movie-misc-info">
-        <li class="year">Year: ${details.Year}</li>
-        <li class="rated">Ratings: ${details.Rated}</li>
-        <li class="released">Released: ${details.Released}</li>
-      </ul>
-      <p class="genre"><b>Genre:</b> ${details.Genre}</p>
-      <p class="writer"><b>Writer:</b> ${details.Writer}</p>
-      <p class="actors"><b>Actors: </b>${details.Actors}</p>
-      <p class="plot"><b>Plot:</b> ${details.Plot}</p>
-      <p class="language"><b>Language:</b> ${details.Language}</p>
-      <p class="awards"><b><i class="fas fa-award"></i></b> ${details.Awards}</p>
-    </div>
-  `;
-
-  // Store selected media details in localStorage
-  localStorage.setItem('selectedMedia', JSON.stringify(details));
-}
-
-// Load media from API
+// Load movies/series from API
 async function loadMedia(searchTerm) {
   const url = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${searchTerm}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  if (response.ok && data.results && data.results.length > 0) {
+  const res = await fetch(url);
+  const data = await res.json();
+  if (data.results && data.results.length > 0) {
     searchResults = data.results;
-    localStorage.setItem('searchResults', JSON.stringify(searchResults));
     displayMediaList(searchResults);
   }
 }
 
 // JavaScript code
 // ...
-function findMedia() {
+
+function findMovies() {
   // Get the search input element
   var searchBox = document.getElementById("movie-search-box");
 
@@ -124,80 +72,83 @@ function findMedia() {
   var searchText = searchBox.value.trim();
 
   if (searchText === "") {
-      // If search input is empty, show the random movie heading and hide the search list
-      document.getElementById("search-list").classList.add("hide-search-list");
-      return;
+    // If search input is empty, hide the search list
+    document.getElementById("search-list").classList.add("hide-search-list");
+    return;
   }
 
   // Use the search input value to perform your search logic
-  // ...
+  loadMedia(searchText);
 
   // Once the search results are obtained, display the search list
   document.getElementById("search-list").classList.remove("hide-search-list");
-
-  // Populate the search list with the search results
-  // ...
 }
+
 // ...
 
 function displayMediaList(mediaList) {
   searchList.innerHTML = "";
   mediaList.forEach((media) => {
     const mediaListItem = document.createElement('div');
-    const mediaType = media.title ? 'movie' : 'series';
+    const mediaType = media.media_type; // Get the type of the media
 
-    mediaListItem.dataset.id = media.id;
-    mediaListItem.classList.add('search-list-item');
+    mediaListItem.dataset.mediaId = media.id; // Store the media ID as a data attribute
 
-    let mediaPoster;
-    if (media.poster_path) {
-      mediaPoster = `https://image.tmdb.org/t/p/w500${media.poster_path}`;
-    } else {
-      mediaPoster = "image_not_found.png";
+    if (mediaType === 'movie') {
+      mediaListItem.innerHTML = `
+        <img src="${media.poster_path !== null ? `https://image.tmdb.org/t/p/w500${media.poster_path}` : 'image_not_found.png'}" alt="media poster">
+        <h3>${media.title}</h3>
+      `;
+    } else if (mediaType === 'tv') {
+      mediaListItem.innerHTML = `
+        <img src="${media.poster_path !== null ? `https://image.tmdb.org/t/p/w500${media.poster_path}` : 'image_not_found.png'}" alt="media poster">
+        <h3>${media.name}</h3>
+      `;
     }
 
-    mediaListItem.innerHTML = `
-      <div class="search-item-thumbnail">
-        <img src="${mediaPoster}">
-      </div>
-      <div class="search-item-info">
-        <h3>${media.title || media.name}</h3>
-        <p>Type: ${mediaType}</p>
-        <p>${media.release_date || 'N/A'}</p>
-      </div>
-    `;
+    mediaListItem.addEventListener('click', () => {
+      if (mediaType === 'movie') {
+        getMovieDetails(media.id);
+      } else if (mediaType === 'tv') {
+        getSeriesDetails(media.id);
+      }
+    });
 
     searchList.appendChild(mediaListItem);
   });
-  loadMediaDetails();
 }
 
-function loadMediaDetails() {
-  const searchListMedia = searchList.querySelectorAll('.search-list-item');
-  searchListMedia.forEach((media) => {
-    media.addEventListener('click', async () => {
-      searchList.classList.add('hide-search-list');
-      movieSearchBox.value = "";
-      await getMediaDetails(media.dataset.id);
-    });
-  });
-}
+// Function to display media details
+function displayMediaDetails(details) {
+  const mediaId = details.id;
+  let videoLink;
 
-// Event listener for clicking outside the search box
-window.addEventListener('click', (event) => {
-  if (event.target.id !== 'movie-search-box') {
-    searchList.classList.add('hide-search-list');
+  if (details.mediaType === 'movie') {
+    videoLink = `https://2embed.org/embed/movie?imdb=${mediaId}`;
+  } else if (details.mediaType === 'series') {
+    videoLink = `https://vidsrc.me/embed/${mediaId}`;
+  } else {
+    // Handle other types (if needed)
+    videoLink = '';
   }
-});
 
-// Check if search results are stored in localStorage
-if (localStorage.getItem('searchResults')) {
-  searchResults = JSON.parse(localStorage.getItem('searchResults'));
-  displayMediaList(searchResults);
+  resultGrid.innerHTML = `
+    <div class="media-poster">
+      <img src="${details.poster_path !== null ? `https://image.tmdb.org/t/p/w500${details.poster_path}` : 'image_not_found.png'}" alt="media poster">
+    </div>
+    <div class="media-info">
+      <h3 class="media-title">${details.title || details.name}</h3>
+      ${videoLink ? `<iframe width="100%" height="270" src="${videoLink}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>` : ''}
+      <ul class="media-misc-info">
+        <li class="release-date">Release Date: ${details.release_date || 'N/A'}</li>
+        <li class="rating">Rating: ${details.vote_average || 'N/A'}</li>
+        <li class="author">Author: ${details.author || 'N/A'}</li>
+        <li class="cast">Cast: ${details.cast || 'N/A'}</li>
+        <li class="awards">Awards: ${details.awards || 'N/A'}</li>
+      </ul>
+    </div>
+  `;
+
+  // Store selected media details in localStorage
+  localStorage.setItem('selectedMedia', JSON.stringify(details));
 }
-
-// Event listener for clicking on the logo
-const logo = document.getElementById('logo');
-logo.addEventListener('click', () => {
-  loadRandomMedia();
-});
