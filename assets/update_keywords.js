d1dd0b4ae53ad8cbefd42a07fa144f2e7414fce3
xzr3 <ammaryousaf341@gmail.com>
2023-07-02 21:15:00 +0500

    f

--- assets/update_keywords.js
+++ assets/update_keywords.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
+const path = require("path");
 
 // Read the keywords from the text file
-const keywords = fs.readFileSync("keywords.txt", "utf-8").split("\n");
+const keywords = fs.readFileSync(path.join(__dirname, "keywords.txt"), "utf-8").split("\n");
 
 // Randomly select 15 keywords
 const selectedKeywords = [];
@@ -10,8 +11,10 @@ for (let i = 0; i < 15; i++) {
   selectedKeywords.push(keywords[randomIndex]);
 }
 
+// Specify the relative path to your HTML file
+const htmlFilePath = path.join(__dirname, "../index.html");
+
 // Update the meta keywords in the HTML file
-const htmlFilePath = ["../index.html","../series.html","../movies.html"]; // Replace with the actual path to your HTML file
 let htmlContent = fs.readFileSync(htmlFilePath, "utf-8");
 htmlContent = htmlContent.replace(/<meta\s+name="keywords"\s+id="metaKeywords"\s+content="[^"]*"/, `<meta name="keywords" id="metaKeywords" content="${selectedKeywords.join(", ")}"`);
 fs.writeFileSync(htmlFilePath, htmlContent);
