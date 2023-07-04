e9cf6fc892de0d57614b4cd055d8961a4d313271
xzr3 <ammaryousaf341@gmail.com>
2023-07-04 17:19:16 +0500

    dd

--- assets/update_keywords.js
+++ assets/update_keywords.js
@@ -4,13 +4,6 @@ const path = require("path");
 // Read the keywords from the text file
 const keywords = fs.readFileSync(path.join(__dirname, "keywords.txt"), "utf-8").split("\n");
 
-// Randomly select 15 keywords
-const selectedKeywords = [];
-for (let i = 0; i < 20; i++) {
-  const randomIndex = Math.floor(Math.random() * keywords.length);
-  selectedKeywords.push(keywords[randomIndex]);
-}
-
 // Specify the relative paths to your HTML files
 const htmlFiles = [
   path.join(__dirname, "../index.html"),
@@ -18,14 +11,28 @@ const htmlFiles = [
   path.join(__dirname, "../series.html")
 ];
 
+// Function to select random keywords
+function selectRandomKeywords(numKeywords) {
+  const selectedKeywords = [];
+  for (let i = 0; i < numKeywords; i++) {
+    const randomIndex = Math.floor(Math.random() * keywords.length);
+    selectedKeywords.push(keywords[randomIndex]);
+  }
+  return selectedKeywords;
+}
+
 // Update the meta keywords in each HTML file
 htmlFiles.forEach((htmlFilePath) => {
+  const numKeywords = 20; // Set the desired number of keywords here
+  const selectedKeywords = selectRandomKeywords(numKeywords);
+
   let htmlContent = fs.readFileSync(htmlFilePath, "utf-8");
   htmlContent = htmlContent.replace(/<meta\s+name="keywords"\s+id="metaKeywords"\s+content="[^"]*"/, `<meta name="keywords" id="metaKeywords" content="${selectedKeywords.join(", ")}"`);
   fs.writeFileSync(htmlFilePath, htmlContent);
+
+  console.log(`Meta keywords updated successfully for ${htmlFilePath}!`);
 });
 
 console.log("Meta keywords updated successfully for all files!");
 
-//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
 
